6397e5b2a30ebf904b8be7d96400c94a93373efe
Critical <criticalimpact@gmail.com>
2024-07-09 18:41:19 +1000

    Add missing interface and class

---
+++ DalaMock.Shared/Classes/DalamudWindowSystem.cs
@@ -0,0 +1,55 @@
+﻿namespace DalaMock.Shared.Classes;
+
+using System.Collections.Generic;
+using DalaMock.Shared.Interfaces;
+using Dalamud.Interface.Windowing;
+
+public class DalamudWindowSystem : IWindowSystem
+{
+    /// <summary>
+    /// The dalamud window system.
+    /// </summary>
+    public WindowSystem WindowSystem { get; private set; }
+
+    public DalamudWindowSystem(string? imNamespace = null)
+    {
+        this.WindowSystem = new WindowSystem(imNamespace);
+    }
+
+    /// <inheritdoc/>
+    public IReadOnlyList<Window> Windows => this.WindowSystem.Windows;
+
+    /// <inheritdoc/>
+    public bool HasAnyFocus => this.WindowSystem.HasAnyFocus;
+
+    /// <inheritdoc/>
+    public string? Namespace
+    {
+        get => this.WindowSystem.Namespace;
+        set => this.WindowSystem.Namespace = value;
+    }
+
+    /// <inheritdoc/>
+    public void AddWindow(Window window)
+    {
+        this.WindowSystem.AddWindow(window);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveWindow(Window window)
+    {
+        this.WindowSystem.RemoveWindow(window);
+    }
+
+    /// <inheritdoc/>
+    public void RemoveAllWindows()
+    {
+        this.WindowSystem.RemoveAllWindows();
+    }
+
+    /// <inheritdoc/>
+    public virtual void Draw()
+    {
+        this.WindowSystem.Draw();
+    }
+}
---
+++ DalaMock.Shared/Interfaces/IWindowSystem.cs
@@ -0,0 +1,49 @@
+﻿namespace DalaMock.Shared.Interfaces;
+
+using System.Collections.Generic;
+using Dalamud.Interface.Windowing;
+
+public interface IWindowSystem
+{
+    /// <summary>
+    /// Gets a read-only list of all <see cref="Window"/>s in this <see cref="WindowSystem"/>.
+    /// </summary>
+    IReadOnlyList<Window> Windows { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether any window in this <see cref="WindowSystem"/> has focus and is
+    /// not marked to be excluded from consideration.
+    /// </summary>
+    bool HasAnyFocus { get; }
+
+    /// <summary>
+    /// Gets or sets the name/ID-space of this <see cref="WindowSystem"/>.
+    /// </summary>
+    string? Namespace { get; set; }
+
+    /// <summary>
+    /// Add a window to this <see cref="WindowSystem"/>.
+    /// The window system doesn't own your window, it just renders it
+    /// You need to store a reference to it to use it later.
+    /// </summary>
+    /// <param name="window">The window to add.</param>
+    void AddWindow(Window window);
+
+    /// <summary>
+    /// Remove a window from this <see cref="WindowSystem"/>.
+    /// Will not dispose your window, if it is disposable.
+    /// </summary>
+    /// <param name="window">The window to remove.</param>
+    void RemoveWindow(Window window);
+
+    /// <summary>
+    /// Remove all windows from this <see cref="WindowSystem"/>.
+    /// Will not dispose your windows, if they are disposable.
+    /// </summary>
+    void RemoveAllWindows();
+
+    /// <summary>
+    /// Draw all registered windows using ImGui.
+    /// </summary>
+    void Draw();
+}

455849ee4a5ea336434ace36d7978f673e30c328
Critical <criticalimpact@gmail.com>
2024-07-10 17:40:42 +1000

    2.0.6

--- DalaMock/Mocks/MockKeyState.cs
+++ DalaMock/Mocks/MockKeyState.cs
@@ -1,123 +1,155 @@
-// using Dalamud.Game.ClientState.Keys;
-// using Dalamud.Plugin.Services;
-//
-// namespace DalaMock.Dalamud;
-//
-// using System;
-// using System.Collections.Generic;
-// using SDL2;
-//
-// public class MockKeyState : IKeyState, IDisposable, IMockService
-// {
-//     private readonly Sdl2Window _window;
-//
-//     public MockKeyState(Sdl2Window window)
-//     {
-//         _window = window;
-//         _window.KeyDown += WindowOnKeyDown;
-//         _window.KeyUp += WindowOnKeyUp;
-//     }
-//
-//     private void WindowOnKeyUp(SDL.SDL_KeyboardEvent keyEvent)
-//     {
-//         var keyState = keyEvent.ToKeyState();
-//         this[keyState] = false;
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Shift))
-//         {
-//             this[VirtualKey.SHIFT] = false;
-//         }
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Control))
-//         {
-//             this[VirtualKey.CONTROL] = false;
-//         }
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Alt))
-//         {
-//             this[VirtualKey.MENU] = false;
-//         }
-//     }
-//
-//     private void WindowOnKeyDown(KeyEvent keyEvent)
-//     {
-//         var keyState = keyEvent.ToKeyState();
-//         this[keyState] = true;
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Shift))
-//         {
-//             this[VirtualKey.SHIFT] = true;
-//         }
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Control))
-//         {
-//             this[VirtualKey.CONTROL] = true;
-//         }
-//         if (keyEvent.Modifiers.HasFlag(ModifierKeys.Alt))
-//         {
-//             this[VirtualKey.MENU] = true;
-//         }
-//     }
-//
-//     private HashSet<VirtualKey> _activeKeys { get; } = new HashSet<VirtualKey>();
-//     public bool this[int vkCode]
-//     {
-//         get => _activeKeys.Contains((VirtualKey)vkCode);
-//         set
-//         {
-//             if (value)
-//             {
-//                 _activeKeys.Add((VirtualKey)vkCode);
-//             }
-//             else
-//             {
-//                 _activeKeys.Remove((VirtualKey)vkCode);
-//             }
-//         }
-//     }
-//
-//     public bool this[VirtualKey vkCode]
-//     {
-//         get => this[(int)vkCode];
-//         set => this[(int)vkCode] = value;
-//     }
-//
-//     public int GetRawValue(int vkCode)
-//     {
-//         return 0;
-//     }
-//
-//     public int GetRawValue(VirtualKey vkCode)
-//     {
-//         return 0;
-//     }
-//
-//     public void SetRawValue(int vkCode, int value)
-//     {
-//     }
-//
-//     public void SetRawValue(VirtualKey vkCode, int value)
-//     {
-//     }
-//
-//     public bool IsVirtualKeyValid(int vkCode)
-//     {
-//         return true;
-//     }
-//
-//     public bool IsVirtualKeyValid(VirtualKey vkCode)
-//     {
-//         return true;
-//     }
-//
-//     public IEnumerable<VirtualKey> GetValidVirtualKeys() => (VirtualKey[])Enum.GetValuesAsUnderlyingType<VirtualKey>();
-//
-//
-//     public void ClearAll()
-//     {
-//         _activeKeys.Clear();
-//     }
-//
-//     public void Dispose()
-//     {
-//         _window.KeyDown -= WindowOnKeyDown;
-//     }
-//
-//     public string ServiceName => "Key State";
-// }
+namespace DalaMock.Core.Mocks;
+
+using System;
+using System.Collections.Generic;
+using DalaMock.Core.Extensions;
+using Dalamud.Game.ClientState.Keys;
+using Dalamud.Plugin.Services;
+using Veldrid;
+using Veldrid.Sdl2;
+
+/// <summary>
+/// Provides a mock version of dalamuds key state.
+/// </summary>
+public class MockKeyState : IKeyState, IDisposable, IMockService
+{
+    private const int MaxKeyCode = 0xF0;
+    private readonly Sdl2Window window;
+    private readonly HashSet<VirtualKey> activeKeys = new();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MockKeyState"/> class.
+    /// </summary>
+    /// <param name="window">The mock SDL2 window.</param>
+    public MockKeyState(Sdl2Window window)
+    {
+        this.window = window;
+        this.window.KeyDown += this.WindowOnKeyDown;
+        this.window.KeyUp += this.WindowOnKeyUp;
+    }
+
+    /// <inheritdoc/>
+    public string ServiceName => "Key State";
+
+    /// <inheritdoc/>
+    public bool this[VirtualKey vkCode]
+    {
+        get => this[(int)vkCode];
+        set => this[(int)vkCode] = value;
+    }
+
+    /// <inheritdoc/>
+    public bool this[int vkCode]
+    {
+        get => this.activeKeys.Contains((VirtualKey)vkCode);
+        set
+        {
+            if (value)
+            {
+                this.activeKeys.Add((VirtualKey)vkCode);
+            }
+            else
+            {
+                this.activeKeys.Remove((VirtualKey)vkCode);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public int GetRawValue(int vkCode)
+    {
+        return 0;
+    }
+
+    /// <inheritdoc/>
+    public int GetRawValue(VirtualKey vkCode)
+    {
+        return 0;
+    }
+
+    /// <inheritdoc/>
+    public void SetRawValue(int vkCode, int value)
+    {
+    }
+
+    /// <inheritdoc/>
+    public void SetRawValue(VirtualKey vkCode, int value)
+    {
+    }
+
+    /// <inheritdoc/>
+    public bool IsVirtualKeyValid(int vkCode)
+        => this.ConvertVirtualKey(vkCode) != 0;
+
+    /// <inheritdoc/>
+    public bool IsVirtualKeyValid(VirtualKey vkCode)
+        => this.IsVirtualKeyValid((int)vkCode);
+
+
+    /// <inheritdoc/>
+    public IEnumerable<VirtualKey> GetValidVirtualKeys() => (VirtualKey[])Enum.GetValuesAsUnderlyingType<VirtualKey>();
+
+
+    /// <inheritdoc/>
+    public void ClearAll()
+    {
+        this.activeKeys.Clear();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.window.KeyDown -= this.WindowOnKeyDown;
+    }
+
+    private byte ConvertVirtualKey(int vkCode)
+    {
+        if (vkCode <= 0 || vkCode >= MaxKeyCode)
+        {
+            return 0;
+        }
+
+        return (byte)vkCode;
+    }
+
+    private void WindowOnKeyUp(KeyEvent keyEvent)
+    {
+        var keyState = keyEvent.ToKeyState();
+        this[keyState] = false;
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Shift))
+        {
+            this[VirtualKey.SHIFT] = false;
+        }
+
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Control))
+        {
+            this[VirtualKey.CONTROL] = false;
+        }
+
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Alt))
+        {
+            this[VirtualKey.MENU] = false;
+        }
+    }
+
+    private void WindowOnKeyDown(KeyEvent keyEvent)
+    {
+        var keyState = keyEvent.ToKeyState();
+        this[keyState] = true;
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Shift))
+        {
+            this[VirtualKey.SHIFT] = true;
+        }
+
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Control))
+        {
+            this[VirtualKey.CONTROL] = true;
+        }
+
+        if (keyEvent.Modifiers.HasFlag(ModifierKeys.Alt))
+        {
+            this[VirtualKey.MENU] = true;
+        }
+    }
+}
 

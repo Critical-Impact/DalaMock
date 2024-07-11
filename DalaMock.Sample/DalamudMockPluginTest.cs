1db926162425c0f84f8469f32076104207c013fa
Critical <criticalimpact@gmail.com>
2024-07-11 22:47:51 +1000

    Update sample

---
+++ DalaMock.Sample/DalamudMockPluginTest.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="DalamudMockPluginTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace DalaMock.Sample;
+
+using Autofac;
+using DalaMock.Core.Mocks;
+using DalaMock.Core.Windows;
+using DalaMock.Shared.Interfaces;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+public class DalamudMockPluginTest : DalamudPluginTest
+{
+    public DalamudMockPluginTest(IDalamudPluginInterface pluginInterface, IPluginLog pluginLog, ICommandManager commandManager, ITextureProvider textureProvider) : base(pluginInterface, pluginLog, commandManager, textureProvider)
+    {
+    }
+
+    public override void ConfigureContainer(ContainerBuilder containerBuilder)
+    {
+        base.ConfigureContainer(containerBuilder);
+        containerBuilder.RegisterType<MockWindowSystem>().As<IWindowSystem>();
+        containerBuilder.RegisterType<MockFileDialogManager>().As<IFileDialogManager>();
+    }
+}
---
+++ DalaMock.Sample/TestService.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="TestService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace DalaMock.Sample;
+
+using System.Threading;
+using System.Threading.Tasks;
+using DalaMock.Host.Factories;
+using DalaMock.Shared.Interfaces;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Microsoft.Extensions.Hosting;
+
+public class TestService : IHostedService
+{
+    private readonly WindowSystemFactory windowSystemFactory;
+    private readonly IDalamudPluginInterface pluginInterface;
+
+    public TestService(WindowSystemFactory windowSystemFactory, IDalamudPluginInterface pluginInterface)
+    {
+        this.windowSystemFactory = windowSystemFactory;
+        this.pluginInterface = pluginInterface;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
